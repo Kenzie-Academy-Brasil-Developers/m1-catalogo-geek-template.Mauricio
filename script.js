08c3a2a7cb1e83b9606eec6b4449b29b729badb1
Mauricio Abdala <mauricio.abdala@outlook.com>
2023-05-07 18:56:45 -0300

    atualizado

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const productsArray = [
+const productsArray  = [
   // Produto 1
   {
     image: "./assets/img/paintings/quadro-c3po.jpg",
@@ -8,10 +8,80 @@ const productsArray = [
   },
   // Produto 2
   {
+    image: "./assets/img/paintings/quadro-relogio.jpg",
+    name: "Smart Watch",
+    price: "R$120,00",
+    type: "Paintings", 
+  },
+  // Produto 3 (adicione os produtos restantes abaixo)
+  {
+    image: "./assets/img/paintings/quadro-controle.jpg",
+    name: "Controle playstation",
+    price: "R$99,00",
+    type: "Paintings",
+  },
+  //produto 4
+  {
+    image: "./assets/img/paintings/quadro-pacman.jpg",
+    name: "PacMan",
+    price: "R$160,00",
+    type: "Paintings",
+  },
+  //produto 5
+  {
+    image: "./assets/img/paintings/quadro-mario.jpg",
+    name: "Mario",
+    price: "R$275,00",
+    type: "Paintings",
+  },
+  //produto 6
+  {
+    image: "./assets/img/paintings/quadro-pacman.jpg",
+    name: "PacMan",
+    price: "R$160,00",
+    type: "Paintings",
+  },
+  //produto 7
+  {
     image: "./assets/img/actions/dragonball-action-figure.jpg",
     name: "Goku Super Saiyajin",
     price: "R$500,00",
-    type: "Action Figures",
+    type: "ActionFigures",
   },
-  // Produto 3 (adicione os produtos restantes abaixo)
+  //produto 8
+  {
+    image: "./assets/img/actions/godzilla-action-figure.jpg",
+    name: "Godzilla",
+    price: "R$ 700,00",
+    type: "ActionFigures",
+  },
+  //produto 9
+  {
+    image: "./assets/img/actions/groot-action-figure.jpg",
+    name: "Groot",
+    price: "R$950,00",
+    type: "ActionFigures",
+  },
+  //produto 10
+  {
+    image: "./assets/img/actions/ironman-action-figure.jpg",
+    name: "Homem de Ferro",
+    price: "R$ 1.030,00",
+    type: "ActionFigures",
+  },
+  //produto 11
+  {
+    image: "./assets/img/actions/starwars-action-figure.jpg",
+    name: "Sr Yoda",
+    price: "R$870,00",
+    type: "ActionFigures"
+  },
+  //produto 12
+  {
+    image: "assets/img/actions/wolverine-action-figure.jpg",
+    name: " Wolverine",
+    price: "R$510,00",
+    type: "ActionFigures"
+  },
+  
 ];
